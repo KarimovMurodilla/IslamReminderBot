1543b013ca1a041299ed56f2ef7ac65a09273f08
KarimovMurodilla <karimovmurodilla15@gmail.com>
2022-03-19 22:04:50 +0500

    Updated version

--- app/buttons.py
+++ app/buttons.py
@@ -1,7 +1,7 @@
 from aiogram import types
 from loader import db
 
-
+# This is inline keyboard will be showed when user send command /dashboard
 def show_dashboard(user_id):
 	groups = db.get_my_groups(user_id)
 	dashboard = types.InlineKeyboardMarkup(row_width = 2)
@@ -15,11 +15,33 @@ def show_dashboard(user_id):
 	return dashboard
 
 
-def show_group_settings(group_id, status):
-	settings = types.InlineKeyboardMarkup()
-	r_status = types.InlineKeyboardButton(text = f'Namoz vaqtini eslatish {status}', callback_data = f'change_rStatus {group_id}')
-	btn_back = types.InlineKeyboardMarkup(text = "⬅️ Orqaga", callback_data = "back")
-	settings.add(r_status)
+def show_group_settings(group_id, status_remind):
+	settings = types.InlineKeyboardMarkup(row_width = 2)
+	r_status = types.InlineKeyboardButton(text = f'Namoz vaqtini eslatish {status_remind}', callback_data = f'change_rStatus {group_id}')
+	set_location = types.InlineKeyboardButton(text = f'Mintaqani sozlash', callback_data = f'set_location')
+	set_audio = types.InlineKeyboardButton(text = f'Azon Ovozi', callback_data = f'show_audios {group_id}')
+	b_status = types.InlineKeyboardButton(text = f'Yozishni cheklash', callback_data = f'b_status')
+	btn_back = types.InlineKeyboardMarkup(text = "⬅️ Orqaga", callback_data = "back_to_dashboard")
+	settings.add(
+		r_status, set_location,
+		set_audio, b_status
+		)
 	settings.add(btn_back)
 
 	return settings
+
+
+def show_audios(group_id, a_status):
+	audios = db.get_azan_audios()
+	audio_dashboard = types.InlineKeyboardMarkup(row_width = 2)
+	for i in range(len(audios)):
+		i += 1
+		audio_btn = types.InlineKeyboardButton(text = f"{i} {a_status}", callback_data = f"audio_id {i}")
+		audio_dashboard.add(audio_btn)
+
+	random_mode = types.InlineKeyboardMarkup("Random", callback_data = f"set_audio_to_random_mode {group_id}")
+	btn_back = types.InlineKeyboardMarkup(text = "⬅️ Orqaga", callback_data = f"back_to_settings {group_id}")
+	audio_dashboard.add(random_mode)
+	audio_dashboard.add(btn_back)
+
+	return audio_dashboard
--- app/handlers/callbacks.py
+++ app/handlers/callbacks.py
@@ -8,15 +8,20 @@ from app import buttons
 async def callback_get_group_settings(c: types.CallbackQuery):
 	user_id = c.from_user.id
 	group_id = c.data[9:]
+	group_name = db.get_data_from_group_id(group_id)[2]
 
 	await c.answer()
-	await c.message.edit_text("Guruxingiz sozlamalari", 
-        reply_markup = buttons.show_group_settings(group_id, db.get_data_from_group_id(group_id)[5]))
+	await c.message.edit_text(f"<b>{group_name}</b> guruxining sozlamalari:", 
+		reply_markup = buttons.show_group_settings(
+			group_id = group_id, 
+			status_remind = db.get_data_from_group_id(group_id)[5]))
 
 
 async def callback_change_rstatus(c: types.CallbackQuery):
 	user_id = c.from_user.id
 	group_id = c.data[15:]
+	group_name = db.get_data_from_group_id(group_id)[2]
+
 	if db.get_data_from_group_id(group_id)[5] == '🟢':
 		db.update_rStatus(group_id, '🔴')
 		await c.answer("O'chdi")
@@ -24,17 +29,43 @@ async def callback_change_rstatus(c: types.CallbackQuery):
 		db.update_rStatus(group_id, '🟢')
 		await c.answer("Yondi")
 
-	await c.message.edit_text("Guruxingiz sozlamalari", 
-        reply_markup = buttons.show_group_settings(group_id, db.get_data_from_group_id(group_id)[5]))
+	await c.message.edit_text(f"<b>{group_name}</b> guruxining sozlamalari:", 
+		reply_markup = buttons.show_group_settings(
+			group_id = group_id, 
+			status_remind = db.get_data_from_group_id(group_id)[5]))
 
 
-async def callback_back_to_menu(c: types.CallbackQuery):
-    user_id = c.from_user.id
-    await c.message.edit_text("Assalomu aleykum\nSizning guruxlaringiz ro'yhati", 
-        reply_markup = buttons.show_dashboard(user_id))
+async def callback_show_audios(c: types.CallbackQuery):
+	await c.answer()
+	group_id = c.data[12:]
+	await c.message.edit_text("Azon ovozlarining ro'yhati:",
+		reply_markup = buttons.show_audios(group_id))
+
+
+async def callback_back_to_settings(c: types.CallbackQuery):
+	user_id = c.from_user.id
+	group_id = c.data[17:]
+	group_name = db.get_data_from_group_id(group_id)[2]
+	
+	await c.answer()
+	await c.message.edit_text(f"<b>{group_name}</b> guruxining sozlamalari:", 
+		reply_markup = buttons.show_group_settings(
+			group_id = group_id, 
+			status_remind = db.get_data_from_group_id(group_id)[5]))
+
+
+async def callback_back_to_dashboard(c: types.CallbackQuery):
+	user_id = c.from_user.id
+
+	await c.answer()
+	await c.message.edit_text("Sizning guruxlaringiz ro'yhati", 
+		reply_markup = buttons.show_dashboard(user_id))
 
 
 def register_callback_handlers(dp: Dispatcher):
 	dp.register_callback_query_handler(callback_get_group_settings, lambda c: c.data.startswith('group_id'), state = '*')
 	dp.register_callback_query_handler(callback_change_rstatus, lambda c: c.data.startswith('change_rStatus'), state = '*')
-	dp.register_callback_query_handler(callback_back_to_menu, lambda c: c.data == "back", state = '*')
+	dp.register_callback_query_handler(callback_show_audios, lambda c: c.data.startswith('show_audios'), state = '*')
+	
+	dp.register_callback_query_handler(callback_back_to_settings, lambda c: c.data.startswith("back_to_settings"), state = '*')
+	dp.register_callback_query_handler(callback_back_to_dashboard, lambda c: c.data == "back_to_dashboard", state = '*')
